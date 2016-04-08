12505df998f5284856095d2827133d45ac37f51b
Ken <ken.joss1@gmail.com>
2016-04-08 18:48:54 +0200

    Version 0.1
    Currently German only!

---
+++ Folder_Rename.ps1
@@ -0,0 +1,21 @@
+#--EDIT--#
+#Verzeichnis INDEM die Ordner erstellt werden sollen
+$path = "F:\Torrents\Serien\Prison Break\Staffel 4"
+
+#--EDIT--#
+#Anzahl der zu erstellenden Ordner
+$ordnerAnzahl = 24
+
+for ($i=1; $i -le $ordnerAnzahl; $i++) {
+    $i2 = "{0:D2}" -f $i
+
+    #--EDIT--#
+    #Namen der Ordner (Episoden "01" durch "$i2" ersetzten | zB. S01E$i2)
+    $ordnerName = "Prison.Break.S04E$i2.German.DL.DTS.BluRay.1080p.x264-x264Crew"
+
+    
+    #Ordner under "$path" erstellen
+    New-Item -ItemType directory -Path $path\$ordnerName
+
+}
+
