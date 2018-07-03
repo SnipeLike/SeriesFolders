419705f6ceb412634ed7ea162ae927b5f8135f20
Ken <ken.joss1@gmail.com>
2018-07-03 11:32:27 +0200

    Improvements
    Improvements & new in english.

--- Folder_Rename.ps1
+++ Folder_Rename.ps1
@@ -1,21 +1,27 @@
 #--EDIT--#
-#Verzeichnis INDEM die Ordner erstellt werden sollen
-$path = "C:\Series\PrisonBreak\S04\"
+#define dir in which the new folders should be created
+$path = "F:\Shows\XXX\Part 1"
 
 #--EDIT--#
-#Anzahl der zu erstellenden Ordner
-$ordnerAnzahl = 24
+#count of folders to create
+$ordnerAnzahl = 12
 
 for ($i=1; $i -le $ordnerAnzahl; $i++) {
     $i2 = "{0:D2}" -f $i
 
     #--EDIT--#
-    #Namen der Ordner (Episoden "01" durch "$i2" ersetzten | zB. S01E$i2)
-    $ordnerName = "Prison.Break.S04E$i2.German.DL.DTS.BluRay.1080p.x264-x264Crew"
-
+    #folders names (substitute the episode number with "$i2" | Eg. S01E01 --> S01E$i2)
+    $ordnerName = "TVShow.S02E$i2.DD51.DL.1080p.HD.x264-Crewz"
     
-    #Ordner under "$path" erstellen
-    New-Item -ItemType directory -Path $path\$ordnerName
-
+    write-host "Folders will look like: $ordnerName" 
+    
+    $confirmation = Read-Host "Are you Sure You Want To Proceed"
+    if ($confirmation -eq 'y') {
+        #create folders in "$path"
+        New-Item -ItemType directory -Path $path\$ordnerName
+    }
+    else {
+        write-host "Quit"
+        break
+    }
 }
-
